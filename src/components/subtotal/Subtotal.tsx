6de30b4ebf433995d97dd193f7ed7077ab959cc1
Rafiw Idrissu <dev.rafiu@gmail.com>
2023-09-28 16:34:38 -0700

    change currency format in subtotal

--- src/components/subtotal/Subtotal.tsx
+++ src/components/subtotal/Subtotal.tsx
@@ -10,8 +10,8 @@ export const Subtotal: FunctionComponent<Props> = ({ total, totalItems }) => {
   return (
     <div className={classes.subtotal}>
       <p>
-        Subtotal({`${totalItems} ${totalItems > 1 ? `items` : `item`}`} )
-        <span className={classes.amount}>${total}</span>
+        Subtotal ({`${totalItems} ${totalItems > 1 ? `items` : `item`}`} )
+        <span className={classes.amount}>GHS {total}</span>
       </p>
     </div>
   );
